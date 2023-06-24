021211ed9783fe1df9d99fe0e4e242bb2dca1139
Evelina Neumann <evelina.neumann1@gmail.com>
2023-06-24 13:03:08 +0200

    changed model

--- pet-App/server/models/User.model.js
+++ pet-App/server/models/User.model.js
@@ -12,7 +12,7 @@ const userSchema = new Schema(
     },
     password: {
       type: String,
-      required: [true, "Password is required."],
+      required: [flase, "Password is required."],
     },
     name: {
       type: String,
